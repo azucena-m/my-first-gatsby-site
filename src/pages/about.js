2f43a24de4695f9d08e675029d19241510e478bd
Azucena Marroquin <atrevizo4@cnm.edu>
2026-07-14 13:22:17 -0600

    Finished part 3 of the tutorial

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 
 const AboutPage = () => {
+
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
